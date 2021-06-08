4b436f9d7f7dcc413b301536c747902ef18f8526
Itallo007 <italosantos.oficial2000@gmail.com>
2021-06-07 21:10:02 -0300

    Cria a estrutura inicial do projeto.
    criado com 'create-react-app'. 'react-router-dom' foi instalado e
    a configuração básica foi feita. Alguns arquivos de teste foram
    deletados para deixar o projetos mais limpo.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Routes from './routes';
+
+function App() {
+  return (
+    // Routes carregará as pages de acordo com a rota atual.
+    // Veja o arquivo routes.jsx
+    <Routes />
+  );
+}
+
+export default App;
