8dea54d3e7580d4032ceaf921633719c861c02f5
mvictor10 <mvictordossantos.limite@gmail.com>
2021-06-10 18:57:37 -0300

    Update inputUsername.jsx
    Atualização do arquivo somente.

--- src/components/InputComponent/inputUsername.jsx
+++ src/components/InputComponent/inputUsername.jsx
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import './inputs.css';
 
-const tag_nick = 'Digite o seu login';
+const tag_username = 'Usuário';
 
 export default class InputUser extends Component{
     render(){
         return(
-            <input className="inputUser" type="text" placeholder={tag_nick}/>
+            <input className="inputUser" type="text" placeholder={tag_username}/>
         )
     }
 }
