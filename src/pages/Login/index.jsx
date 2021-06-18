084fa4fc1005274b51aeb399b26e184a426d5e0f
mvictor10 <mvictordossantos.limite@gmail.com>
2021-06-18 17:56:29 -0300

    Update index.jsx
    Atualização de arquivos

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,14 +1,16 @@
+
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
 import InputUser from '../../components/InputComponent';
 import Button from '../../components/buttons';
 
-import logo from '../../icon/logotipo_sava.svg';
 import lock from '../../icon/lock-icon.svg';
 import IconFacebook from '../../icon/facebook';
 import IconTwitter from '../../icon/twitter';
 import IconIfba from '../../icon/ifba';
+import Logotipo from '../../icon/logotipo_sava.svg';
+
 
 
 import './login.css';
@@ -23,7 +25,7 @@ export default class LoginPages extends Component{
             <div className="App">
                 <header className="App-header">
                     <form className="App-form">
-                        <img className="App-icone" src={logo} alt=""/>
+                        <img className="App-icone" src={Logotipo} alt=""/>
                         <div className="App-line">
                             <svg xmlns="http://www.w3.org/2000/svg" className="lock-icon" viewBox="0 0 20 20" fill="currentColor">
                                 <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
@@ -35,17 +37,19 @@ export default class LoginPages extends Component{
                             type="text"
                             placeholder="Login:"
                         />
+
                         <InputUser
-                            name="password"
+                            name="senha"
                             type="password"
                             placeholder="Senha:"
                         />
+                        
                         <Button 
                             name="Acessar"
                             type="submit"
                         />
                         <br/>
-                        <Link to="/recuperar-senha" className="App-link">Esqueçeu a senha?</Link>
+                        <Link to="/voltar" className="App-link">Voltar</Link>
                         </form>
                 </header>
                 <footer className="App-footer">
@@ -60,6 +64,9 @@ export default class LoginPages extends Component{
     }
 
 }
+
+
+
 
 
 
