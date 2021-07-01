2d6e508617b308fdbc3127985cfbf97e5c547d0a
Itallo007 <italosantos.oficial2000@gmail.com>
2021-07-01 01:11:55 -0300

    Modifica o a Login page
    implementa as functios que tratam o submit e o change dos inputs

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 
 import './login.css';
@@ -13,37 +13,62 @@ import IconFacebook from '../../icon/facebook';
 import IconTwitter from '../../icon/twitter';
 import IconIfba from '../../icon/ifba';
 
-export default class LoginPages extends Component{
+export default function LoginPages() {
+    const [user, setUser] = useState({login: '', password: ''});
 
-    render(){
-        return(
-           <div className="AppRoot">
-               <header className="App">
-                    <form>
-                        <img className="App-icone" src={Logotipo} alt=""/>
-                        <div className="linha">
-                            <IconLock className="iconLock" />
-                            <h5>Login</h5>
-                        </div>
-                        <Input type="text" placeholder="Login: "/>
-                        <Input type="password" placeholder="Senha: "/>
-                        <Button type="submit" name="Enviar"/>
-                        <br />              
-                        <Link to="/recuperar-senha" className="App-link">Recuperar Conta?</Link>
-                    </form>
-               </header>
-               <footer className="rodape">
-                   <h5 className="rodape_titulo">&copy; 2021 SAVA | Desenvolvimento IFBA.</h5>
-                   <div className="linha_rodape">
-                       <IconFacebook />
-                       <IconTwitter />
-                       <IconIfba />
-                   </div>
-               </footer>
-           </div>
-        );
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        console.log(user);
     }
 
+    // salva os dados dos inputs no state
+    function handleInputChange(event) {
+        setUser({
+            ...user,
+            [event.target.name]: event.target.value
+        });
+    }
+    
+    return (
+        <div className="AppRoot">
+            <header className="App">
+                 <form onSubmit={handleSubmit} >
+                     <img className="App-icone" src={Logotipo} alt=""/>
+                     <div className="linha">
+                         <IconLock className="iconLock" />
+                         <h5>Login</h5>
+                     </div>
+                     <Input
+                        name="login"
+                        type="text"
+                        placeholder="Login: "
+                        value={user.login}
+                        onChange={handleInputChange}
+                    />
+
+                     <Input
+                        name="password"
+                        type="password"
+                        placeholder="Senha: "
+                        value={user.password}
+                        onChange={handleInputChange}
+                    />
+                     <Button type="submit" name="Enviar"/>
+                     <br />              
+                     <Link to="/recuperar-senha" className="App-link">Recuperar Conta?</Link>
+                 </form>
+            </header>
+            <footer className="rodape">
+                <h5 className="rodape_titulo">&copy; 2021 SAVA | Desenvolvimento IFBA.</h5>
+                <div className="linha_rodape">
+                    <IconFacebook />
+                    <IconTwitter />
+                    <IconIfba />
+                </div>
+            </footer>
+        </div>
+    );
 }
 
 
