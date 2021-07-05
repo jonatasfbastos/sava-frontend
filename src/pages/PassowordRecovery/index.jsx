5a7be287e03600cf8bcb073533b713bd400ba92d
abelbcarvalho <abelbdecarvalho@gmail.com>
2021-07-05 12:16:07 -0300

    estilização password recovery tela 1

--- src/pages/PassowordRecovery/index.jsx
+++ src/pages/PassowordRecovery/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import './style.css'
 import logo from '../../icon/logotipo_sava.svg';
-
+import IconLock from '../../icon/lock';
 import IconFacebook from '../../icon/facebook';
 import IconTwitter from '../../icon/twitter';
 import IconIfba from '../../icon/ifba';
@@ -15,9 +15,10 @@ class PasswordRecovery extends React.Component {
           <form action="" class="fundo-form">
             <div class="content">
               <div>
-                <img className="App-icone sava" src={logo} alt=""/>
+                <img className="App-icone sava logo-sava" src={logo} alt=""/>
               </div>
               <div class="div-recupera">
+                <IconLock className="iconLock" />
                 <p>Recuperar Conta</p>
               </div>
               <input type="text" name="" id="" class="login-entrada" placeholder="Login:" />
@@ -31,11 +32,11 @@ class PasswordRecovery extends React.Component {
           <footer className="App-footer">
             <div class="content">
               <h5 className="App-subtitle centraliza">&copy; 2021 SAVA | Desenvolvimento IFBA.</h5>
-                <div class="social-m">
-                  <IconFacebook className="App-facebook icon"/>
-                  <IconTwitter className="App-twitter icon"/>
-                  <IconIfba className="App-ifba icon" />
-                </div>
+              <div class="social-m">
+                <IconFacebook className="App-facebook icon"/>
+                <IconTwitter className="App-twitter icon"/>
+                <IconIfba className="App-ifba icon" />
+              </div>
             </div>
           </footer>
       </div>
