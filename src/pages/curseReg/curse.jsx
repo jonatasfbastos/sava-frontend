9b6d997b1206c84d81207530a68df0373cd5d916
mvictor10 <mvictordossantos.limite@gmail.com>
2021-07-06 02:19:00 -0300

    Create curse.jsx

---
+++ src/pages/curseReg/curse.jsx
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+
+import "./curse.css";
+
+import Input from '../../components/InputComponent';
+import Button from '../../components/buttons';
+
+import IconLock from '../../icon/lock';
+
+
+function RegistrarCurso(){
+    return(
+        <div className="AppRoot-curse">
+            <header className="App-curse">
+                <div className="App-curse-icone-close">
+                    <a  className="App-curse-close" href="#">X</a>
+                </div>
+                 <form>   
+                     <div className="linha-curse">
+                         <h5>Adicionar Curso</h5>
+                     </div>
+                     <Input type="text" placeholder="Nome"/>
+                     <Input type="text" placeholder="Código"/>
+                     <Button type="submit" name="Confirmar"/>
+                     <br />              
+                 </form>
+            </header>
+        </div>
+    )
+}
+
+export default RegistrarCurso;
+
+
+
+
