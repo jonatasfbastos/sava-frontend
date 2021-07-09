4d68523ef1df406c3b88118f4de4d299a78b86c8
Itallo007 <italosantos.oficial2000@gmail.com>
2021-07-09 18:25:35 -0300

    Implementa a tela de cadastro de turma
    O HTML e o CSS da tela de cadastro de turma foi criado e algumas
    modificações foram feitas no css do InputComponent e Button

--- src/pages/NewClass/index.jsx
+++ src/pages/NewClass/index.jsx
@@ -1,10 +1,53 @@
+import {Link} from 'react-router-dom';
+
+import Input from '../../components/InputComponent';
+import Button from '../../components/buttons';
 
 import './styles.css';
+import Logo from '../../icon/logotipo.svg';
 
 export function NewClass() {
+  const etapas = ['MatrizINFO - Etapa 1', 'MatrizINFO - Etapa 2', 'MatrizINFO - Etapa 3'];
+
   return (
     <div id="new-class">
-      <h1>Cadastro de turma</h1>
+      <main>
+        <div className="main-header">
+          <img src={Logo} alt="Logo sava" />
+        </div>
+        
+        <form >
+          <label>
+            {/* Etapa: */}
+            <select
+              required
+              id="select-etapa"
+              name="etapa"
+              type="select"
+            >
+              <option value="" disabled selected hidden>Selecione a etapa</option>
+              {etapas.map((etapa, index)=> (
+                <option key={index} value={index + 1}>{etapa}</option>
+              ))}
+            </select>
+          </label>
+
+          <Input
+            name="anoLetivo"
+            type="date"
+            placeholder="Ano Letivo"
+          />
+          
+          <Input
+            name="nome"
+            type="text"
+            placeholder="Nome da turma"
+          />
+
+          <Button type="submit" >Salvar</Button>
+        </form>
+        <Link to='/' >Voltar</Link>
+      </main>
     </div>
   );
 }
