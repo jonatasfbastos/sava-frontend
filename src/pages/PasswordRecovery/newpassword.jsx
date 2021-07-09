c4b9b4bb6182d1609f0b1496a64fdf4ab54f61f4
Itallo007 <italosantos.oficial2000@gmail.com>
2021-07-08 22:59:26 -0300

    Implementa os metodos que manipulam a alteração da senha

--- src/pages/PasswordRecovery/newpassword.jsx
+++ src/pages/PasswordRecovery/newpassword.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import './style.css'
+import Button from '../../components/buttons';
+import InputComponent from '../../components/InputComponent/index'
+import api from '../../services/api';
 import logo from '../../icon/logotipo_sava.svg';
 import IconLock from '../../icon/lock';
 import IconFacebook from '../../icon/facebook';
@@ -8,11 +11,53 @@ import IconTwitter from '../../icon/twitter';
 import IconIfba from '../../icon/ifba';
 
 class PasswordRecovery extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newPassword: '',
+      confirmNewPassword: ''
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.checkPasswords = this.checkPasswords.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    
+    if(!this.checkPasswords()) {
+      alert("Senhas não compatíveis");
+      return;
+    }
+
+    window.alert("Tudo ok, mas ainda não :(");
+
+    // const {newPassword} = this.state;
+    
+    // api.patch('/users', {
+    //   newPassword
+    // })
+
+  }
+
+  handleInputChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  checkPasswords() {
+    const {newPassword, confirmNewPassword} = this.state;
+
+    return newPassword.trim() === confirmNewPassword.trim();
+  }
 
   render() {
     return(
       <div className="pass-recovery">
-          <form action="" class="fundo-form">
+          <form onSubmit={this.handleSubmit} class="fundo-form">
             <div class="content">
               <div>
                 <img className="App-icone sava logo-sava" src={logo} alt=""/>
@@ -21,9 +66,34 @@ class PasswordRecovery extends React.Component {
                 <IconLock className="iconLock" />
                 <p>Recuperar Conta</p>
               </div>
-              <input type="password" name="" id="" class="login-entrada marg-bottom-15" placeholder="Nova senha" />
-              <input type="password" name="" id="" class="login-entrada marg-bottom-15" placeholder="Confirmar senha" />
-              <button className="recupera-botao marg-top-30">Enviar</button>
+              <InputComponent
+                minlength="8"
+                required
+                type="password"
+                name="newPassword"
+                class="login-entrada marg-bottom-15"
+                placeholder="Nova senha"
+                value={this.state.newPassword}
+                onChange={this.handleInputChange}
+              />
+
+              <InputComponent
+                minlength="8"
+                required
+                type="password"
+                name="confirmNewPassword"
+                class="login-entrada marg-bottom-15"
+                placeholder="Confirmar senha"
+                value={this.state.confirmNewPassword}
+                onChange={this.handleInputChange}
+              />
+
+              <Button
+                type="submit"
+                className="recupera-botao marg-top-30"
+              >
+                Enviar
+              </Button>
               <div class="center-block">
                 <Link to='/login' class='link-voltar'>Voltar</Link>
               </div>
