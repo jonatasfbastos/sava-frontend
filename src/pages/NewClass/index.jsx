97bd450a80be4d87da12c3b47b174488a3badcdd
Itallo007 <italosantos.oficial2000@gmail.com>
2021-07-11 02:35:23 -0300

    Implementa o castro de turma
    cria a lógica para pegar os dados da turma e savar na  base. Alguns
    dados foram adicionados no db.json.

--- src/pages/NewClass/index.jsx
+++ src/pages/NewClass/index.jsx
@@ -1,5 +1,7 @@
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
+import api from '../../services/api';
 import Input from '../../components/InputComponent';
 import Button from '../../components/buttons';
 
@@ -7,7 +9,64 @@ import './styles.css';
 import Logo from '../../icon/logotipo.svg';
 
 export function NewClass() {
-  const etapas = ['MatrizINFO - Etapa 1', 'MatrizINFO - Etapa 2', 'MatrizINFO - Etapa 3'];
+  const [stepsList, setStepsList] = useState([]);
+  const [stepId, setStepId] = useState(-1);
+  const [year, setYear] = useState('');
+  const[name, setName] = useState('');
+
+  const history = useHistory();
+
+  // Executa assim que o component é renderizado
+  useEffect(() => {
+    setSteps();
+  }, []);
+
+  // Defini o nome da turma
+  useEffect(() => {
+    let step = stepsList.find(stepItem => stepItem.id === stepId);
+
+    if(step && year) {
+      setName(`${step.name} - ${year}`);
+    }
+  },[stepsList, stepId, year]);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    
+    const clazz = {
+      name,
+      year,
+      stepId
+    }
+
+    // Salva na base
+    api.post('/classes', clazz).then(response => {
+      window.confirm('Cadastrado com sucesso!');
+      history.push('/');
+    }).catch(error => {
+      console.error(error);
+    })
+
+  }
+
+  // Busca as etapas disponíveis
+  function setSteps() {
+    api.get('/steps?_expand=matrix').then(response => {
+      setStepsList(response.data)
+    }).catch(error => {
+      console.error(error);
+    })
+  }
+
+  // Gera uma lista de anos
+  function getYearsList() {
+    let list = [];
+    let y = new Date().getFullYear();
+    for(let i=y;i >= 2010;i--) {
+      list.push(String(i));
+    }
+    return list;
+  }
 
   return (
     <div id="new-class">
@@ -16,7 +75,7 @@ export function NewClass() {
           <img src={Logo} alt="Logo sava" />
         </div>
         
-        <form >
+        <form onSubmit={handleSubmit}>
           <label>
             {/* Etapa: */}
             <select
@@ -24,24 +83,56 @@ export function NewClass() {
               id="select-etapa"
               name="etapa"
               type="select"
+              value={stepId}
+              onChange={e => setStepId(Number.parseInt(e.target.value))}
             >
-              <option value="" disabled selected hidden>Selecione a etapa</option>
-              {etapas.map((etapa, index)=> (
-                <option key={index} value={index + 1}>{etapa}</option>
+              <option
+                disabled
+                defaultValue
+                // selected
+                hidden
+                value={-1}
+              >
+                Selecione a etapa
+              </option>
+
+              {stepsList.map((step, index)=> (
+                <option key={index} value={step.id}>{step.name}</option>
               ))}
             </select>
           </label>
 
-          <Input
-            name="anoLetivo"
-            type="date"
-            placeholder="Ano Letivo"
-          />
+          <label>
+            <select
+              required
+              id="select-year"
+              name="year"
+              type="select"
+              value={year}
+              onChange={e => setYear(e.target.value)}
+            >
+              <option
+                disabled
+                defaultValue
+                // selected
+                hidden
+                value=""
+              >
+                Selecione o ano letivo
+              </option>
+
+              {getYearsList().map(((year, index) => (
+                  <option key={index} value={year}>{year}</option>
+              )))}
+            </select>
+          </label>
           
           <Input
             name="nome"
             type="text"
             placeholder="Nome da turma"
+            disabled
+            value={name}
           />
 
           <Button type="submit" >Salvar</Button>
